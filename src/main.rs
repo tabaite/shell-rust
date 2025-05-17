9dcaa9673c4e576e09b4e071a86b1ea352c9aaf0
tabaite <105614378+tabaite@users.noreply.github.com>
2025-05-17 12:17:28 -0500

    Formatting, started work on shell running

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::process::Command;
 #[allow(unused_imports)]
-use std::{io::{self, Write}, env};
+use std::{
+    env,
+    io::{self, Write},
+};
 
 fn main() {
     // This doesn't really feel good, but we'll deal with this later.
@@ -11,32 +15,33 @@ fn main() {
             for path in env::split_paths(&paths) {
                 let dir_result = std::fs::read_dir(path);
                 match dir_result {
-                    Ok(dir_iter) => for file_result in dir_iter {
-                        match file_result {
-                            Ok(entry) => {
-                                let path = entry.path();
-                                let stem = path.file_stem().unwrap().to_str();
-                                if stem.is_some() {
-                                    let name = stem.unwrap();
-                                    if !path_map.contains_key(name) {
-                                        path_map.insert(
-                                        name
-                                                .to_owned(),
-                                            path.as_os_str().to_str().unwrap().to_owned(),
-                                        );
+                    Ok(dir_iter) => {
+                        for file_result in dir_iter {
+                            match file_result {
+                                Ok(entry) => {
+                                    let path = entry.path();
+                                    let stem = path.file_stem().unwrap().to_str();
+                                    if stem.is_some() {
+                                        let name = stem.unwrap();
+                                        if !path_map.contains_key(name) {
+                                            path_map.insert(
+                                                name.to_owned(),
+                                                path.as_os_str().to_str().unwrap().to_owned(),
+                                            );
+                                        }
                                     }
                                 }
-                            },
-                            Err(_) => {}
+                                Err(_) => {}
+                            }
                         }
                     }
                     // Windows error: file not found
-                    Err(e) if e.raw_os_error() == Some(3) => {},
+                    Err(e) if e.raw_os_error() == Some(3) => {}
                     Err(e) => println!("error: {}", e),
                 }
             }
         }
-        None => println!("no path?????")
+        None => println!("no path?????"),
     }
 
     // Infinite loop for running
@@ -52,7 +57,6 @@ fn main() {
         let mut args_iter = input.trim().split(' ').into_iter();
         let command = args_iter.next();
         match command {
-            // in future, get args
             Some(c) => match c {
                 "" => continue,
                 // hacky, but we'll implement this properly later
@@ -62,27 +66,33 @@ fn main() {
                 },
                 "type" => match args_iter.next() {
                     Some(s) => match s {
-                        builtin if
-                            builtin == "exit" || builtin == "type" || builtin == "echo"
-                            => println!("{} is a shell builtin", builtin),
+                        builtin if builtin == "exit" || builtin == "type" || builtin == "echo" => {
+                            println!("{} is a shell builtin", builtin)
+                        }
                         other => match path_map.get(other) {
                             Some(path) => println!("{} is {}", other, path),
                             None => println!("{}: not found", other),
-                        }
+                        },
                     },
-                    _ => {},
+                    _ => {}
                 },
                 "echo" => {
                     for v in args_iter {
                         print!("{} ", v);
                     }
                     println!("");
-                },
-                other => match path_map.get(other) {
-                    Some(path) => println!("{} is {}", other, path),
-                    None => println!("{}: command not found", input.trim()),
                 }
-            }
+                // TODO: we don't need to get the path, that's expensive
+                other => match path_map.get(other) {
+                    Some(path) => {
+                        println!("{} is {}", other, path);
+
+                        let result =
+                            Command::new(other).args::<Vec<&str>, &str>(args_iter.collect());
+                    }
+                    None => println!("{}: command not found", input.trim()),
+                },
+            },
             None => {
                 continue;
             }
