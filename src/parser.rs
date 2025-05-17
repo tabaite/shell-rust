f5bf288c80a144228db67a66039fe4ab6c894607
tabaite <105614378+tabaite@users.noreply.github.com>
2025-05-17 15:41:13 -0500

    Add support for single quotes

--- src/parser.rs
+++ src/parser.rs
@@ -28,6 +28,16 @@ pub fn test_split_args_whitespace() {
     assert_eq!("fat", args.next().unwrap());
 }
 
+#[test]
+pub fn test_split_args_single_quotes() {
+    let string = "'cat sat' 'bat' 'fat'";
+    let mut args = SplitArgsIter::new(string);
+
+    assert_eq!("cat sat", args.next().unwrap());
+    assert_eq!("bat", args.next().unwrap());
+    assert_eq!("fat", args.next().unwrap());
+}
+
 impl<'a> Iterator for SplitArgsIter<'a> {
     type Item = &'a str;
 
@@ -35,15 +45,43 @@ impl<'a> Iterator for SplitArgsIter<'a> {
         if self.current_index >= self.length {
             return None;
         }
+
+        // Indicates the seperator.
+        #[derive(PartialEq, Eq)]
+        enum ParamType {
+            Whitespace,
+            SingleQuoted,
+            DoubleQuoted,
+        }
+
+        let mut current_param_type = ParamType::Whitespace;
+
         let bytes = self.backing_str.as_bytes();
         for i in self.current_index..self.length {
-            if bytes[i] == b' ' {
-                let result = Some(unsafe { str::from_utf8_unchecked(&bytes[self.current_index..i]) });
-                // We want to start at the space AFTER we encounter the space.
-                // If this causes current index to be greater than the length,
-                // it's still safe.
-                self.current_index = i+1;
-                return result;
+            let current_char = bytes[i];
+            match current_char {
+                //                                                     we don't want a bunch of blank params
+                b' ' if current_param_type == ParamType::Whitespace && self.current_index != i => {
+                    let result = Some(unsafe { str::from_utf8_unchecked(&bytes[self.current_index..i]) });
+                    // We want to start at the space AFTER we encounter the space.
+                    // If this causes current index to be greater than the length,
+                    // it's still safe.
+                    self.current_index = i+1;
+                    return result;
+                },
+                b'\'' if current_param_type == ParamType::Whitespace => {
+                    current_param_type = ParamType::SingleQuoted;
+                    self.current_index = i+1;
+                },
+                b'\'' if current_param_type == ParamType::SingleQuoted => {
+                    let result = Some(unsafe { str::from_utf8_unchecked(&bytes[self.current_index..i]) });
+                    // We want to start at the space AFTER we encounter the space.
+                    // If this causes current index to be greater than the length,
+                    // it's still safe.
+                    self.current_index = i+1;
+                    return result;
+                }
+                _ => {}
             }
         }
         // We've reached the end
